250230748cb5606a92f75fac24d7ba50eed229f9
opaque-maniac <mbithimark8@gmail.com>
2024-05-10 20:54:12 +0300

    Added cors

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,24 @@ import {
 } from "./middleware/authMiddleware";
 import router from "./router";
 import ErrorHandler from "./utils/errorHandler";
+import cors from "cors";
 
 const app = express();
 
+// Implimenting some middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+// Options for cors
+const corsOptions = {
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+
+// Implimenting cors
+app.use(cors(corsOptions));
 
 app.get("/register", (req, res) => {
   res.send("Hello World");
